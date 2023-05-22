1aa8e14363da3f944137bb40f5fe87320391bf04
Alexander Nicholi <me@nicholatian.com>
2023-05-22 17:55:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Oración assembler
+
+Copyright © 2023 Alexander Nicholi.
+All rights reserved.
+
+-----
+
+This is a barebones Python implementation of the Oración assembler, used to bootstrap the self-hosting version of the assembler written in Oración assembly language for DOS.
---
+++ oracion.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+
+HELP_TEXT = '''
+Oracion assembler
+Copyright (C) 2023 Alexander Nicholi
+
+Usage:-
+\toracion <input> <output>
+'''
+
+def main(args):
+	argc = len(args)
+	if argc != 3 or '-h' in args or '--help' in args:
+		print(HELP_TEXT)
+		return 0
+	f = open(args[1], 'rb')
+	indata = f.read().decode('utf-8')
+	f.close()
+	
+	return 0
+
+if __name__ == '__main__':
+	from sys import argv, exit
+	exit(main(argv))
