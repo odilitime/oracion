c5e80658c179403ed6bbf42b9933a27095d02371
Alexander Nicholi <me@nicholatian.com>
2023-08-25 14:17:22 -0400

    add more modeopts

--- src/getopt2.h
+++ src/getopt2.h
@@ -1,6 +1,8 @@
 
 #include "minihn.h"
 
+typedef u8 modeopt_t;
+
 enum
 {
 	/* single-dash short flags and double-dash long flags are used. */
@@ -11,8 +13,16 @@ enum
 	GETOPT2_MODEOPT_MASK_VERBS = 0x4,
 	/* one dash for stdin; two for flag parser termination. */
 	GETOPT2_MODEOPT_MASK_DASHES = 0x8,
+	/* whether the form --flag=value is recognised. */
+	GETOPT2_MODEOPT_MASK_LFLAGEQ = 0x10,
+	/* whether short flags stack together -likeso. */
+	GETOPT2_MODEOPT_MASK_SFLAGSTACK = 0x20,
+	/* whether successive flags override previous ones left-to-right
+	 * (it is otherwise treated as an error to have multiple conflicting
+	 * flags) */
+	GETOPT2_MODEOPT_MASK_LTROVR = 0x40,
 	/* mask of all reserved bits (must all be LOW). */
-	GETOPT2_MODEOPT_MASK_RESERVED = 0xF0
+	GETOPT2_MODEOPT_MASK_RESERVED = 0x80
 };
 
 struct getopt2_args
@@ -27,10 +37,24 @@ struct getopt2_flagparam
 	chr * value;
 };
 
-struct getopt2_args getopt2_normalise( int, char ** );
+struct getopt2_args getopt2_normalise( int argc, char ** argv )
+{
+	struct getopt2_args r;
 
-struct getopt2_flagparam getopt2_getflag( u8, chr,
-	chr *, struct getopt2_args );
+	r.argc = (ptri)argc;
+	r.argv = (chr **)argv;
+
+	return r;
+}
+
+struct getopt2_flagparam getopt2_getflag( modeopt_t modeopt, chr sflag,
+	chr * lflag, struct getopt2_args args )
+{
+	ptri i;
+
+	for(i = 0; i < args.argc; ++i)
+	{}
+}
 
 chr * getopt2_getverb( u8, struct getopt2_args );
 
